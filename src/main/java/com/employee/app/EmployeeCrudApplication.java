a55b2d990f04fdce052f9a9cfe1766bbd2692457
Mohan <divay.mohan@teradata.com>
2021-08-25 09:49:39 +0530

    deployable api

--- src/main/java/com/employee/app/EmployeeCrudApplication.java
+++ src/main/java/com/employee/app/EmployeeCrudApplication.java
@@ -2,9 +2,17 @@ package com.employee.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmployeeCrudApplication {
+	
+	@GetMapping("/app")
+	public String Main() {
+		return "This is my app..!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeCrudApplication.class, args);
